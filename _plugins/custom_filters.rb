88ef7159af50e9a4a78364e3b3617a78b646395f
Jeff Hull <jeff@fitocracy.com>
2014-09-10 09:42:36 -0400

    update comments

--- _plugins/custom_filters.rb
+++ _plugins/custom_filters.rb
@@ -1,6 +1,9 @@
 module Jekyll
   module MyFilters
-    # assumes file is in /css/
+    # Gets Md5 contents of file (assumed to be in /css/)
+    # and returns the hash to be appended to any CSS file in <head>
+    # when used with css file, automatically makes sure that deployed CSS 
+    # is cachebust every time the file changes
     def md5_cache_bust(file_name)
       require 'digest/md5'
       '/css/' + file_name + '?' + Digest::MD5.file('css/' + file_name).hexdigest
